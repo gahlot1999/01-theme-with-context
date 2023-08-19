c7f18a26cf7d4bd459bd340c6ccb802f2239fb05
gahlot1999 <gahlot1999@gmail.com>
2023-08-19 23:14:07 +0530

    starter file

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+function App() {
+  return (
+    <div className='main'>
+      <button className='theme-btn'>☀️</button>
+      <nav className='navbar'>
+        <ul>
+          <li>
+            <a href='#'>Home</a>
+          </li>
+          <li>
+            <a href='#'>About Us</a>
+          </li>
+          <li>
+            <a href='#'>Contact</a>
+          </li>
+          <li>
+            <a href='#'>Login</a>
+          </li>
+        </ul>
+      </nav>
+
+      <div className='hero'>
+        <h1>Sample Website</h1>
+      </div>
+
+      <button>Click Me</button>
+    </div>
+  );
+}
+
+export default App;
